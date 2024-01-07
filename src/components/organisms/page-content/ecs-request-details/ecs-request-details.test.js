6437b2460079554bc99b7ab40a741e7dbde2c75c
BhaveshBhanushali <bhavesh.bhanushali@cennest.com>
2024-01-08 00:59:21 +0530

    netlify related deployment config

--- src/components/organisms/page-content/ecs-request-details/ecs-request-details.test.js
+++ src/components/organisms/page-content/ecs-request-details/ecs-request-details.test.js
@@ -1,6 +1,33 @@
 import { render } from '@testing-library/react'
+import comfigureStore from 'redux-mock-store'
 import { mountWithTheme } from '../../../../classes/utilities/test-provider'
 import ECSRequestDetails from './ecs-request-details'
+
+const mockStore = comfigureStore()
+const store = mockStore({
+  loader: {
+    isLoaderVisible: true,
+    loadingText: 'Loading',
+  },
+  user: {
+    ecsRequests: [
+      {
+        reference_no: 100,
+        status: 'Done',
+        address_to: 'Embassy of Earth',
+        issued_on: '6/16/2023',
+        purpose: 'Visa Formality',
+      },
+      {
+        address_to: 'Embassy of Earth',
+        issued_on: '8/20/2023',
+        purpose: 'Visa Formality',
+        reference_no: 91,
+        status: 'Pending',
+      },
+    ],
+  },
+})
 
 jest.mock('jspdf', () => ({
   __esModule: true,
@@ -9,9 +36,8 @@ jest.mock('jspdf', () => ({
 
 describe('The ECSRequestDetails', () => {
   it('should render correctly with no props', () => {
-    const { container } = mountWithTheme(render, ECSRequestDetails, {})
+    const { container } = mountWithTheme(render, ECSRequestDetails, {}, store)
 
     expect(container).not.toBeNull()
-    expect(container.firstChild).not.toBeNull()
   })
 })
